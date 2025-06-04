147e2a96130a01f367f4d25ea254119629eae976
hibiscus <yuy0815@icloud.com>
2025-06-04 11:32:52 +0900

    first commit

---
+++ Source/UnrealLab/UnrealLab.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealLab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealLab, "UnrealLab" );
